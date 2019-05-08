2bb9c3741fecf27be4fa956f72a765a7847bb331
hgabriel1 <40220421+hgabriel1@users.noreply.github.com>
2019-05-07 22:51:02 -0300

    Add files via upload

---
+++ CalculatorAdapter/src/calculatoradapter/br/com/calc/ICalculator.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatoradapter.br.com.calc;
+
+/**
+ *
+ * @author 0422
+ */
+
+
+public interface ICalculator {
+
+	public double sum (double a, double b);
+	
+}
